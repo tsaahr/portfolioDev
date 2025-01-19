cad5ae5dd84b9454c108314c83d94bf2a78854a8
tsaahr <lithy.10@hotmail.com>
2025-01-19 09:41:42 -0300

    Update game.js

--- projects/moquito_killer/game.js
+++ projects/moquito_killer/game.js
@@ -1,1 +1,10 @@
+ var _Height = 0
+ var _Width = 0
+ 
+ function ajustaTamanho(){
+     _Height = window.innerHeight
+     _Width = window.innerWidth
+     console.log(_Height, _Width)
+ }
 
+ ajustaTamanho()
